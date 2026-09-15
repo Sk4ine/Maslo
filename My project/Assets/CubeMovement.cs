ec914d62d621a0ba4f6d6314d79f828a1a654d2c
Skaine <sk4inee@gmail.com>
2026-09-15 20:04:13 +0300

    Added Unity project

---
+++ My project/Assets/CubeMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CubeMovement : MonoBehaviour
+{
+    private InputAction _moveAction;
+    
+    private Vector2 _playerInputDirection;
+    private Vector3 _movementVelocity;
+    public float movementSpeed;
+    
+    private void Start()
+    {
+         _moveAction = InputSystem.actions.FindAction("Move");
+    }
+    
+    private void Update()
+    {
+        _playerInputDirection = _moveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        _movementVelocity = new Vector3(_playerInputDirection.x, 0, _playerInputDirection.y) * movementSpeed;
+        gameObject.transform.position += _movementVelocity * Time.deltaTime;
+    }
+}
